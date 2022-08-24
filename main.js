c701b2f715078ebb1e2a586d6dec9b8347eb818f
Prince Ahams <67928653+Princedera@users.noreply.github.com>
2022-08-24 14:30:41 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var dropdownlink = document.getElementById("dropdown-link");
+const hide = document.getElementById("hide");
+
+hide.style.display = "none"
+
+dropdownlink.addEventListener("click", (event) => {
+    if (hide.style.display == "none") {
+        hidetwo.style.display = "none"
+        hide.style.display = "block";
+    }
+    else hide.style.display = "none";
+});
+
+var dropdownlink = document.getElementById("dropdown-link-two");
+var hidetwo = document.getElementById("hidetwo");
+
+hidetwo.style.display = "none";
+
+dropdownlink.addEventListener("click", (event) => {
+    if (hidetwo.style.display == "none") {
+        hide.style.display = "none" 
+        hidetwo.style.display = "block";
+    }
+    else hidetwo.style.display = "none";
+});
