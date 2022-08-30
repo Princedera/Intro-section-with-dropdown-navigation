b11de79749ce5335d600cc5e7fe6d3f46da79f1a
eustacedike <eustacedyke@gmail.com>
2022-08-30 12:16:38 +0100

    Changes
    + added hover effects

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-var dropdownlink = document.getElementById("dropdown-link");
-const hide = document.getElementById("hide");
+// var dropdownlink = document.getElementById("dropdown-link");
+// const hide = document.getElementById("hide");
 
-hide.style.display = "none"
+// hide.style.display = "none"
 
-dropdownlink.addEventListener("click", (event) => {
-    if (hide.style.display == "none") {
-        hidetwo.style.display = "none"
-        hide.style.display = "block";
-    }
-    else hide.style.display = "none";
-});
+// dropdownlink.addEventListener("click", (event) => {
+//     if (hide.style.display == "none") {
+//         hidetwo.style.display = "none"
+//         hide.style.display = "block";
+//     }
+//     else hide.style.display = "none";
+// });
 
-var dropdownlink = document.getElementById("dropdown-link-two");
-var hidetwo = document.getElementById("hidetwo");
+// var dropdownlink = document.getElementById("dropdown-link-two");
+// var hidetwo = document.getElementById("hidetwo");
 
-hidetwo.style.display = "none";
+// hidetwo.style.display = "none";
 
-dropdownlink.addEventListener("click", (event) => {
-    if (hidetwo.style.display == "none") {
-        hide.style.display = "none" 
-        hidetwo.style.display = "block";
-    }
-    else hidetwo.style.display = "none";
-});
+// dropdownlink.addEventListener("click", (event) => {
+//     if (hidetwo.style.display == "none") {
+//         hide.style.display = "none" 
+//         hidetwo.style.display = "block";
+//     }
+//     else hidetwo.style.display = "none";
+// });
